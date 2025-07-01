990315520ff1d47884be45efd5c71febfcda7778
Cristina <cris78seijas@gmail.com>
2025-07-01 12:34:24 -0300

    se agrego meta data

--- code.js
+++ code.js
@@ -10,4 +10,14 @@ items.forEach((item, i) => {
   }
 });
 
+// Desactiva clic derecho
+  document.addEventListener('contextmenu', event => event.preventDefault());
 
+  // Desactiva algunas teclas de inspección
+  document.onkeydown = function(e) {
+    if (e.keyCode === 123 || // F12
+        (e.ctrlKey && e.shiftKey && (e.keyCode === 73 || e.keyCode === 74)) || // Ctrl+Shift+I/J
+        (e.ctrlKey && e.keyCode === 85)) { // Ctrl+U
+      return false;
+    }
+  };
